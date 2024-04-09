dbb95d2ec116ea7f3cc10c06105c9ad1585d0031
Alfredo Urdaneta <alfredourdaneta@Alfredos-MBP.attlocal.net>
2024-04-09 14:57:20 -0400

    Added: Data Fetching, Car Card Component, Car Details Modal Component

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,8 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import CarCard from "./CarCard";
 
-export { CustomButton, Footer, Hero, Navbar };
+export { CarCard, CustomButton, CustomFilter, Footer, Hero, Navbar, SearchBar };
